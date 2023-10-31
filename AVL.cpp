9ed83fc2b98d49d74c5077a56534b33ef00be082
cristhoferChow <cristhoferchow9@gmail.com>
2023-10-31 11:38:15 -0300

    Rotações feitas

--- AVL.cpp
+++ AVL.cpp
@@ -207,13 +207,32 @@ NO* insereArvore(NO* no, int valor)
 NO* girarDireita(NO* no)
 {
 	// sua implementação vai aqui
-	return no;
+	NO* aux1 = no->esq;
+	NO* aux2 = aux1->dir;
+
+	aux1->dir = no;
+	no->esq = aux2;
+
+	no->altura = max(alturaNo(no->esq), alturaNo(no->dir)) + 1;
+	aux1->altura = max(alturaNo(no->esq), alturaNo(no->dir)) + 1;
+
+	return aux1;
 }
 
 NO* girarEsquerda(NO* no)
 {
 	// sua implementação vai aqui
-	return no;
+
+	NO* aux1 = no->dir;
+	NO* aux2 = aux1->esq;
+
+	aux1->esq = no;
+	no->dir = aux2;
+
+	no->altura = max(alturaNo(no->esq), alturaNo(no->dir)) + 1;
+	aux1->altura = max(alturaNo(no->esq), alturaNo(no->dir)) + 1;
+
+	return aux1;
 }
 
 int elementosArvore(NO* no)
